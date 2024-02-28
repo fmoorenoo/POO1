7b277d1bc409df332960f74ffe817e9cdb0b149a
fmoorenoo <fermontemore@gmail.com>
2024-02-28 12:35:53 +0000

    Ficheros persona y main_persona acabados

---
+++ main_persona.py
@@ -0,0 +1,10 @@
+from persona import Persona
+
+def main():
+    fer = Persona("Fer", 18)
+    fer.imprimir()
+    fer.cumpleaños()
+
+
+if __name__ == '__main__':
+    main()
--- persona.py
+++ persona.py
@@ -1,13 +1,14 @@
-class persona:
+class Persona:
     def __init__(self, nombre, años):
         self.años = años
         self.nombre = nombre
 
     def imprimir(self):
-        print(f"{self.nombre} tiene {self.años}")
+        print(f"{self.nombre} tiene {self.años} años")
 
     def cumpleaños(self):
-        pass
+        self.años = self.años + 1
+        print(f"Es el cumpleaños de {self.nombre}. Ahora tiene {self.años} años.")
 
 
 def main():
