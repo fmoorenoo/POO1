e9b773f6baffcfd1881477941f70cbea90208d75
fmoorenoo <fermontemore@gmail.com>
2024-02-28 09:06:00 +0000

    Modificaciones

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
-¿Qué palabra reservada se utiliza en lugar de animal (nombre de la clase) para acceder al atributo patas?
-
 class animal:
-    patas = 0
-
-    def caminar():
-        print(f"Caminando con {animal.patas} patas")
+    def caminar(self): #método
+        self.patas = 0 #atributo
+        print(f"Caminando con {self.patas} patas")
 
 def main():
-    vaca = animal
+    vaca = animal()
     vaca.patas = 4
     vaca.caminar()
 
-    pato = animal
-    vaca.patas = 2
-    pato.caminar
+    pato = animal()
+    pato.patas = 2
+    pato.caminar()
+
+    vaca.caminar()
 
 if __name__ == "__main__":
     main()
