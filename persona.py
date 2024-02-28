6d2730df36d769ce8f11318834b78f472ee3066f
fmoorenoo <fermontemore@gmail.com>
2024-02-28 09:46:06 +0000

    Estructura creada del archivo persona

---
+++ persona.py
@@ -0,0 +1,18 @@
+class persona:
+    def __init__(self, nombre, años):
+        self.años = años
+        self.nombre = nombre
+
+    def imprimir(self):
+        print(f"{self.nombre} tiene {self.años}")
+
+    def cumpleaños(self):
+        pass
+
+
+def main():
+     pass
+
+
+if __name__ == '__main__':
+     main()
