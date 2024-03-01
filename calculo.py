987b1a3aa0409aebd8e5b8ed5ad64e4a74df32d9
fmoorenoo <fermontemore@gmail.com>
2024-03-01 13:10:46 +0000

    Programa de calculo actualizado

--- calculo.py
+++ calculo.py
@@ -2,34 +2,34 @@ class Calculo:
     def __init__(self, num1:int, num2:int):
         self.num1 = num1
         self.num2 = num2
-        self.suma = 0
-        self.resta = 0
+        self.sum = 0
+        self.rest = 0
         self.multi = 0
         self.div = 0
 
-    def sumador(self):
-        self.suma = self.num1 + self.num2
+    def suma(self) -> int:
+        self.sum = self.num1 + self.num2
+        return self.sum
 
-    def restador(self):
-        self.resta = self.num1 - self.num2
+    def resta(self) -> int:
+        self.rest = self.num1 - self.num2
+        return self.rest
 
-    def multiplicador(self):
+    def multiplicacion(self) -> int:
         self.multi = self.num1 * self.num2
+        return self.multi
 
-    def divisor(self):
+    def division(self) -> int:
         self.div = self.num1 / self.num2
+        return self.div
 
     def imprimir(self):
-        print(f"Suma: {self.suma}. Resta: {self.resta}. Multiplicación: {self.multi}. División: {self.div}")
-
+        print(f"Suma: {self.suma()}. Resta: {self.resta()}. Multiplicación: {self.multiplicacion()}. División: {self.division()}")
 
 
 if __name__ == '__main__':
     calculo = Calculo(10, 5)
-    calculo.sumador()
-    calculo.restador()
-    calculo.multiplicador()
-    calculo.divisor()
     calculo.imprimir()
+    
     
     
