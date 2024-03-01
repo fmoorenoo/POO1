194eff65bbd28fb703806fb099ae452bdc8f3690
fmoorenoo <fermontemore@gmail.com>
2024-03-01 09:39:44 +0000

    Terminado el programa de cryptos

--- crypto.py
+++ crypto.py
@@ -2,6 +2,8 @@ class Crypto:
     def __init__(self, nombre:str, euros:float):
         self.nombre = nombre
         self.euros = euros
+        self.min = 0.
+        self.max = 0.
 
     def stopLoss(self, min:float, max:float):
         self.min = min
@@ -9,8 +11,10 @@ class Crypto:
 
     def imprimir(self):
         print(f"El valor de {self.nombre} es de {self.euros} €")
+        print(f"Stop Loss. Min: {self.min}. Stop Loss. Max: {self.max}")
 
 
 if __name__ == '__main__':
     bitcoin = Crypto("Bitcoin", 57000)
+    bitcoin.stopLoss(55000, 60000)
     bitcoin.imprimir()
